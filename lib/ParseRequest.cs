bcf038f186996f0fe486653ccf6c3a911593c347
Jordan Luyke <jordanluyke@gmail.com>
2012-09-10 06:24:24 -0700

    added parserequest

---
+++ lib/ParseRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ParseRequest : API_Methods
+{
+    public static string AuthenticateURL(string YOUR_CLIENT_ID, string YOUR_REGISTERED_REDIRECT_URI)
+    {
+        return Uri.EscapeUriString("https://api.put.io/v2/oauth2/authenticate" +
+            "?client_id=" + YOUR_CLIENT_ID +
+            "&response_type=code" +
+            "&redirect_uri=" + YOUR_REGISTERED_REDIRECT_URI);
+    }
+
+    public static string TokenRequestURL(string YOUR_CLIENT_ID, string YOUR_CLIENT_SECRET, string YOUR_REGISTERED_REDIRECT_URI, string CODE)
+    {
+        return Uri.EscapeUriString("https://api.put.io/v2/oauth2/access_token" +
+            "?client_id=" + YOUR_CLIENT_ID +
+            "&client_secret=" + YOUR_CLIENT_SECRET +
+            "&grant_type=authorization_code" +
+            "&redirect_uri=" + YOUR_REGISTERED_REDIRECT_URI +
+            "&code=" + CODE);
+    }
+}
+
+public class API_Methods
+{
+    public static class Files
+    {
+        static int i = 3;
+
+        public static string[] List(string parent_id)
+        {
+            string[] nfo = new string[i];
+            nfo[0] = "GET";
+            nfo[1] = "files/list/" + parent_id;
+            return nfo;
+        }
+
+        public static string[] CreateFolder(string name, string parent_id)
+        {
+            string[] nfo = new string[i];
+            nfo[0] = "POST";
+            nfo[1] = "files/create-folder";
+            nfo[2] = "name=" + name +
+                "&parent_id=" + parent_id;
+            return nfo;
+        }
+    }
+}
--- lib/netputio.cs
+++ lib/netputio.cs
@@ -3,72 +3,73 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.IO;
-using System.Web;
+//using System.Web;
+//using System.Threading;
+using System.Threading.Tasks;
 
 class netputio
 {
-    //public string authRedirect = Uri.EscapeUriString("https://api.put.io/v2/oauth2/authenticate?client_id=175&response_type=code&redirect_uri=https://twitter.com/ethernex");
-    /// <summary>
-    /// Redirects to https://YOUR_REGISTERED_REDIRECT_URI/?code=CODE. Used to obtain CODE.
-    /// </summary>
     public string AuthURL { get; private set; }
     private string clientID;
     private string regRedirectUri;
     private string clientSecret;
     public string Token { get; private set; }
 
-    /// <summary>
-    /// Forms authRedirect URL for client login page.
-    /// </summary>
     public netputio(string YOUR_CLIENT_ID, string YOUR_REGISTERED_REDIRECT_URI, string YOUR_CLIENT_SECRET)
     {
         this.clientID = YOUR_CLIENT_ID;
         this.regRedirectUri = Uri.EscapeUriString(YOUR_REGISTERED_REDIRECT_URI);
         this.clientSecret = YOUR_CLIENT_SECRET;
-        this.AuthURL = Uri.EscapeUriString("https://api.put.io/v2/oauth2/authenticate" +
-            "?client_id=" + clientID +
-            "&response_type=code" +
-            "&redirect_uri=" + regRedirectUri);
+        this.AuthURL = ParseRequest.AuthenticateURL(clientID, regRedirectUri);
         this.Token = null;
     }
 
-    /// <summary>
-    /// Sets public Token variable.
-    /// </summary>
-    /// <param name="CODE"></param>
-    public void TokenRequest(string CODE)
+    public Task TokenRequest(string CODE)
     {
-        string request = Uri.EscapeUriString("https://api.put.io/v2/oauth2/access_token" +
-            "?client_id=" + clientID +
-            "&client_secret=" + clientSecret +
-            "&grant_type=authorization_code" +
-            "&redirect_uri=" + regRedirectUri +
-            "&code=" + CODE);
+        string request = ParseRequest.TokenRequestURL(clientID, clientSecret, regRedirectUri, CODE);
         WebClient wc = new WebClient();
         string[] jsonreturn = wc.DownloadString(request).Split('"');
+        wc.Dispose();
         if (jsonreturn[1] == "access_token")
             this.Token = jsonreturn[3];
         else
             this.Token = null;
+        return null;
     }
 
-    private string jsonParseSend(string param, string value)
+    /*public void Request(string[] method)
     {
-        string s =  "{" +
-                        "\"api_key\":\"" + " " + "\"," +
-                        "\"api_secret\":\"" + " " + "\"," +
-                        "\"params\":{" +
-                        parseparamvalue(param, value) + "," +
-                        parseparamvalue(param, value) +
-                        "}" +
-                    "}";
-        return s;
+        HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create("https://api.put.io/v2/" + method[0]);
+
+        UTF8Encoding encoding = new UTF8Encoding();
+
+        byte[] data = encoding.GetBytes(method[1] + "&oauth_token=" + this.Token);
+
+        httpWReq.Method = "POST";
+        httpWReq.ContentType = "application/x-www-form-urlencoded";
+        httpWReq.ContentLength = data.Length;
+
+        using (Stream newStream = httpWReq.GetRequestStream()) {
+            newStream.Write(data, 0, data.Length);
+        }
+    }*/
+
+    public string Request(string[] mnfo)
+    {
+        string HtmlResult = "";
+        using (WebClient wc = new WebClient()) {
+            string URI = "https://api.put.io/v2/" + mnfo[1];
+            if (mnfo[0] == "GET") {
+                HtmlResult = wc.DownloadString(URI + "?oauth_token=" + this.Token);
+                //HtmlResult = wc.DownloadString("https://api.put.io/v2/files/list?oauth_token=" + this.Token);
+            } else if (mnfo[0] == "POST") {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                mnfo[2] += "&oauth_token=" + this.Token;
+                HtmlResult = wc.UploadString(URI, mnfo[2]);
+            }
+        }
+        return HtmlResult;
     }
 
-    private string parseparamvalue(string param, string value)
-    {
-        return "\"" + param + "\":\"" + value + "\"";
-    }
 
 }
-
